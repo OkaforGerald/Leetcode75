8d40b9a1e76922e700df0d085ed7b77d754a770e
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-08-21 21:54:30 +0100

    Add project files.

---
+++ Leetcode75/1071. Greatest Common Divisor of Strings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75
+{
+    public class Solution
+    {
+        public static string GcdOfStrings(string str1, string str2)
+        {
+            string? prefix = null;
+            for (int i = 1; i <= Math.Min(str1.Length, str2.Length); i++)
+            {
+                var possiblePrefix = str1.Substring(0, i);
+                if (possiblePrefix != str2.Substring(0, i))
+                {
+                    break;
+                }
+                else
+                {
+                    if (CheckStringAgainstPrefix(str1, possiblePrefix))
+                    {
+                        if (CheckStringAgainstPrefix(str2, possiblePrefix)) prefix = possiblePrefix;
+                    }
+                }
+            }
+
+            if (prefix is null) return "";
+
+            return prefix;
+        }
+
+        public static bool CheckStringAgainstPrefix(string str, string prefix)
+        {
+            int i = 0;
+            while (i + prefix.Length - 1 < str.Length)
+            {
+                var subStr = str.Substring(i, prefix.Length);
+                if (subStr != prefix) return false;
+
+                i += prefix.Length;
+            }
+            if (i != str.Length) return false;
+
+            return true;
+        }
+    }
+}
---
+++ Leetcode75/1768. Merge Strings Alternately.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Leetcode75
+//{
+//    public class Solution
+//    {
+//        public string MergeAlternately(string word1, string word2)
+//        {
+//            StringBuilder sb = new();
+//            int limit = Math.Min(word1.Length, word2.Length);
+//            for (int i = 0; i < Math.Min(word1.Length, word2.Length); i++)
+//            {
+//                sb.Append(word1[i]);
+//                sb.Append(word2[i]);
+//            }
+//            if (word1.Length > word2.Length) { sb.Append(word1.Substring(limit)); } else { sb.Append(word2.Substring(limit)); }
+//            return sb.ToString();
+//        }
+//    }
+//}
