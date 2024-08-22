ed0915168dc5da5bf3b368673b4341553566ccfc
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-08-22 20:45:08 +0100

    22nd August

---
+++ Leetcode75/22_August.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75
+{
+        public class Solution
+        {
+            public int FindComplement(int num)
+            {
+                StringBuilder sb = new();
+                int result = 0;
+                while (num != 0)
+                {
+                    var remainder = num % 2;
+                    sb.Append(remainder);
+                    num = num / 2;
+                }
+
+                var binary = sb.ToString().Reverse().ToArray();
+                for (int i = 0; i < binary.Count(); i++)
+                {
+                    if (binary[i] == '1') { binary[i] = '0'; } else { binary[i] = '1'; };
+                }
+                
+                for(int i = 0; i < binary.Length; i++)
+                {
+                    result += (int)char.GetNumericValue(binary[i]) * (int) Math.Pow(2, binary.Length - (i + 1));
+                }
+
+                return result;
+            }
+        }
+    }
