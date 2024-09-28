09680a851c6222dc0bcebae0e87bd08526e9db4f
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-28 16:28:09 +0100

    Solved 2

---
+++ Leetcode75/11. Container With Most Water.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75_11
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int lPointer = 0;
+            int rPointer = height.Length - 1;
+            int LargestArea = 0;
+            while (lPointer <= rPointer)
+            {
+                var CurrentArea = Math.Min(height[lPointer], height[rPointer]) * (rPointer - lPointer);
+                if (CurrentArea > LargestArea) LargestArea = CurrentArea;
+                if (height[rPointer] > height[lPointer]) { lPointer++; } else { rPointer--; }
+            }
+
+            return LargestArea;
+        }
+    }
+}
---
+++ Leetcode75/374. Guess Number Higher or Lower.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode75_374
+{
+    /** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+    public class Solution : GuessGame
+    {
+        public int GuessNumber(int n)
+        {
+            int l = 1;
+            int r = n;
+
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                int result = guess(mid);
+                if (result == 0) return mid;
+
+                if (result == 1)
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
